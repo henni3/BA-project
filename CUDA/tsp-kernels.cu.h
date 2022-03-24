3efa858e1a1a2376f4760a84a5a0b76d9d28b4e2
Henriette Hansen <henni_95@hotmail.com>
2022-03-24 11:39:41 +0100

    Update tsp-kernels.cu.h

--- CUDA/tsp-kernels.cu.h
+++ CUDA/tsp-kernels.cu.h
@@ -10,13 +10,7 @@ __global__ void mkIndShp(int* index_shp_d, int len){
 __global__ void convert(int* out_flags, char* in_flags, int totIter) {
     int glb_id = blockIdx.x * blockDim.x + threadIdx.x;
     if(glb_id < totIter){
-        printf("flag; %d, glb: %d, totIter: %d \n ", in_flags[glb_id], glb_id, totIter);
-        /*if(in_flags[glb_id] == 0){
-            out_flags[glb_id] = 0;
-        }else{
-            out_flags[glb_id] = 1;
-        }*/
-        //=(int)in_flags[glb_id];
+        out_flags[glb_id] = in_flags[glb_id]
     }
 }
 __global__ void replicate0(int totIter, char* flags_d) {
