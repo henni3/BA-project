4d3b9baec7565e2d07c29ea682624778609ef1ff
Henriette Hansen <henni_95@hotmail.com>
2022-04-06 15:31:26 +0200

    Update tsp-main.cu

--- CUDA/tsp-main.cu
+++ CUDA/tsp-main.cu
@@ -140,21 +140,21 @@ int main(int argc, char* argv[]) {
     int* is_h = (int*) malloc(totIter*sizeof(uint32_t));
     cudaMemcpy(is_h, is_d, totIter*sizeof(uint32_t), cudaMemcpyDeviceToHost);
     int k = 0;
-    printf("is: [");
+    /*printf("is: [");
     for(int i = 0; i < totIter; i++){
         printf("%d, ", is_h[i]);
         k++;
     }
     printf("]\n");
-    printf("k = %d\n", k);
+    printf("k = %d\n", k);*/
 
     int* js_h = (int*) malloc(totIter*sizeof(int));
     cudaMemcpy(js_h, js_d, totIter*sizeof(int), cudaMemcpyDeviceToHost);
-    printf("js: [");
+    /*printf("js: [");
     for(int i = 0; i < totIter; i++){
         printf("%d, ", js_h[i]);
     }
-    printf("]\n");
+    printf("]\n");*/
     free(js_h); free(is_h);
     
 
@@ -170,6 +170,7 @@ int main(int argc, char* argv[]) {
     cudaMalloc((void**)&kerTour, (cities+1)*sizeof(unsigned short));
     cudaMemcpy(kerTour, tour, (cities+1)*sizeof(unsigned short), cudaMemcpyHostToDevice);
     unsigned short sharedMemSize = (cities+1) * sizeof(unsigned short) + (block_size*3) * sizeof(int) + 3*sizeof(int);
+    printf("before twoOptKernel \n");
     twoOptKer<<< num_blocks, block_size, sharedMemSize>>> (kerDist, kerTour, is_d, js_d, cities, totIter);
 
     
