58699c445cdc2f6ef852e8e957a07809e3f3169a
Henriette Hansen <henni_95@hotmail.com>
2022-04-07 13:45:58 +0200

    update

--- CUDA/tsp-kernels.cu.h
+++ CUDA/tsp-kernels.cu.h
@@ -111,7 +111,7 @@ __global__ void twoOptKer(uint32_t* glo_dist,
     }
 
 
-    /*Test of shared memory
+    //Test of shared memory
     int resSize = blockDim.x + cities+1;
     int totSize = resSize+3;
     for(i = threadIdx.x; i < totSize; i += blockDim.x){
@@ -132,7 +132,7 @@ __global__ void twoOptKer(uint32_t* glo_dist,
             minChange[tmpM+2] = float (tmpM);
             printf("minChange: fst %f, sec %f, thr %f\n", minChange[tmpM], minChange[tmpM+1], minChange[tmpM+2]);
         }
-    }*/
+    }
     if(idx == 0) {
         printf("check 1 \n");
     }
