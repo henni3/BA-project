91cbf7f46e5abc382d0a2f88a61956d13fae2136
Vonutne <Vonutne@users.noreply.github.com>
2022-04-20 14:38:59 +0200

    test

--- CUDA/tsp-main.cu
+++ CUDA/tsp-main.cu
@@ -199,7 +199,7 @@ int main(int argc, char* argv[]) {
     //printf("sharedmemSize used in twoOptKer : %d \n", sharedMemSize);
     int *glo_results;
     cudaMalloc((void**)&glo_results, 2*restarts*sizeof(int));
-    twoOptKer<<<restarts, block_size, sharedMemSize>>> (kerDist, tourMatrixR_d, 
+    twoOptKer2<<<restarts, block_size, sharedMemSize>>> (kerDist, tourMatrixR_d, 
                                                         is_d, js_d, glo_results, 
                                                         cities, totIter);
     //gpuErrchk( cudaPeekAtLastError() );
