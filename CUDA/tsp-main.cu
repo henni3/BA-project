925e141f7aee48f6cbe7bca7bf4b82e3f5c3b6f0
Henriette Hansen <henni_95@hotmail.com>
2022-04-22 10:44:41 +0200

    Update tsp-main.cu

--- CUDA/tsp-main.cu
+++ CUDA/tsp-main.cu
@@ -6,16 +6,6 @@
 #include "tsp-kernels.cu.h"
 #include "dataCollector.cu.h"
 
-int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
-{
-    unsigned int resolution=1000000;
-    long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec);
-    result->tv_sec = diff / resolution;
-    result->tv_usec = diff % resolution;
-    return (diff<0);
-}
-
-
 int init(int block_size, 
          int cities, 
          int totIter, 
