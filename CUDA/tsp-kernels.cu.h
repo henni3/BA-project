123c1a6e7fbeea4d9e8a4e5604cc99b2810348fb
Henriette Hansen <henni_95@hotmail.com>
2022-04-22 12:27:22 +0200

    Update tsp-kernels.cu.h

--- CUDA/tsp-kernels.cu.h
+++ CUDA/tsp-kernels.cu.h
@@ -107,42 +107,8 @@ __device__ int sumTourKernel2(uint32_t* glo_dist,
 }
 
 //Random tour generator for all restarts, basen on SPLASH-2 code
-//With each thread accessing row wise in the matrix. This does not
-//attcheive coalesced access.
-__global__ void createToursRowWise(unsigned short* tourMatrix, 
-                            int cities,
-                            int restarts,
-                            int time){
-    int rand, glo_id, to, temp;
-    glo_id = threadIdx.x + blockIdx.x * blockDim.x;
-    if(glo_id < restarts){
-        //Initiate all tours from 0 to cities
-        for(int i = 0; i < cities; i++){
-            tourMatrix[(cities+1) * glo_id + i] = i;
-        }
-        //The last element in all tours is the same as the first (which is 0).
-        tourMatrix[(cities+1) * glo_id + cities] = 0;
-
-        //Randomize each tour
-        rand = glo_id + blockIdx.x + time; //blockIdx.x is tourOffset. Check if this is correct
-        for(int i = 1; i < cities; i++){
-            rand = (MULT * rand + ADD) & MASK;
-            to = rand % cities;
-            if (to <= 0){
-                to = 1;
-            }
-            temp = tourMatrix[(cities+1) * glo_id + i];
-            tourMatrix[(cities+1) * glo_id + i] = tourMatrix[(cities+1) * glo_id + to];
-            tourMatrix[(cities+1) * glo_id + to] = temp;
-        }
-    }
-}
-
-
-//Random tour generator for all restarts, basen on SPLASH-2 code
 //With each thread accessing column wise in the matrix to attcheive
 //coalesced access.
-//!!!!!!!!!!!!!!!!!!!!!!!!!!NOOOOT DONE!!!!!!!!!!!!!!!!!!
 __global__ void createToursColumnWise(unsigned short* tourMatrix, 
                             int cities,
                             int restarts,
