534a1e0544526afc301856a5298009b27ae817e9
Henriette Hansen <henni_95@hotmail.com>
2022-04-22 11:02:54 +0200

    Update tsp-main.cu

--- CUDA/tsp-main.cu
+++ CUDA/tsp-main.cu
@@ -196,7 +196,7 @@ int main(int argc, char* argv[]) {
     createToursColumnWise<<<num_blocks_tour, block_size>>> (tourMatrixC_d, cities, restarts);*/
 
     //run 2 opt kernel 
-    //size_t sharedMemSize = (cities+1) * sizeof(unsigned short) + block_size * sizeof(ChangeTuple) + sizeof(ChangeTuple);
+    size_t sharedMemSize = (cities+1) * sizeof(unsigned short) + block_size * sizeof(ChangeTuple) + sizeof(ChangeTuple);
     //printf("sharedmemSize used in twoOptKer : %d \n", sharedMemSize);
     int *glo_results;
     cudaMalloc((void**)&glo_results, 2*restarts*sizeof(int));
