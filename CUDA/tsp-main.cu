d1b8dc8caf08d087316fd8349f2bba6f94583e95
Vonutne <Vonutne@users.noreply.github.com>
2022-05-11 15:35:50 +0200

    make :)

--- CUDA/tsp-main.cu
+++ CUDA/tsp-main.cu
@@ -54,7 +54,7 @@ int main(int argc, char* argv[]) {
     //testing timer for cities 100 program
     REPEAT = 0;
     gettimeofday(&start, NULL); 
-    while(REPEAT < 1){
+    while(REPEAT < 10){
         init(block_size, cities, totIter, is_d, js_d);
 
         //Prepare for column wise tour
