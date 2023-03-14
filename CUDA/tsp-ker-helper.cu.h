99b4d026def642cf522411a9f8a8f37942b790d1
Henriette Hansen <henni_95@hotmail.com>
2023-03-14 17:10:14 +0100

    test

--- CUDA/tsp-ker-helper.cu.h
+++ CUDA/tsp-ker-helper.cu.h
@@ -193,8 +193,8 @@ __global__ void multBlockReduce(int* glo_result,
         __syncthreads();
     }
     if(idx == 0){        
-        glo_result[blockIdx.x*2] = sharedMem[0];
-        glo_result[(blockIdx.x*2)+1] = sharedMem[1];
+        glo_result[blockIdx.x*2] = sharedMem[1];
+        glo_result[(blockIdx.x*2)+1] = sharedMem[0];
     }
 
     /*//Find limit of how many elements are to be reduced by this block.
