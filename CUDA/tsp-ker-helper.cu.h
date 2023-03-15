c870761d4f9707231cfa6e02f069972e6ef9c085
Henriette Hansen <henni_95@hotmail.com>
2023-03-15 11:16:14 +0100

    debugging

--- CUDA/tsp-ker-helper.cu.h
+++ CUDA/tsp-ker-helper.cu.h
@@ -160,10 +160,10 @@ __global__ void multBlockReduce(int* glo_result,
         elem2 = glo_result[(glo_id + block_size)*2];
     }*/
     
-    if(glo_id > num_elems){
-        sharedMem[idx*2] = 2147483647;//INT_MAX;
-        sharedMem[(idx*2)+1] = 2147483647;//INT_MAX;
-    }else if((glo_id + block_size) > num_elems){
+    if(glo_id > (num_elems-1)){
+        sharedMem[idx*2] = INT_MAX;
+        sharedMem[(idx*2)+1] = INT_MAX;
+    }else if((glo_id + block_size) > (num_elems-1)){
         sharedMem[idx*2] = glo_result[(glo_id*2)];
         sharedMem[(idx*2)+1] = glo_result[(glo_id*2)+1];
     }else{
