98a9a62c15f8cbc62beea530683800b6cc017fe8
Henriette Hansen <henni_95@hotmail.com>
2023-03-18 15:45:03 +0100

    test

--- CUDA/tsp-main-helper.cu.h
+++ CUDA/tsp-main-helper.cu.h
@@ -89,9 +89,9 @@ void run_original(unsigned short *tourMatrixIn_d,
                  unsigned short *tourMatrixTrans_d,
                  int *is_d, uint32_t* kerDist, int *glo_results,
                  int block_size, int cities, int restarts, int totIter){
-    /*int num_blocks_restarts;
+    /*int num_blocks_restarts;*/
     num_blocks_restarts = (restarts + block_size-1)/block_size;
-    */
+    
     int num_blocks_tour, time;
     struct timeval randomTime;
 
@@ -135,10 +135,9 @@ void run_100cities(unsigned short *tourMatrixIn_d,
                  int block_size, int cities, int restarts, int totIter){
     int num_blocks_restarts;
     num_blocks_restarts = (restarts + block_size-1)/block_size;
-    
     //Create randomized tours
-    createToursColumnWise<<<num_blocks_restarts, block_size>>> (tourMatrixIn_d, cities, restarts);
-    transposeTiled<unsigned short, TILE>(tourMatrixIn_d, tourMatrixTrans_d, (cities+1), restarts);
+    //createToursColumnWise<<<num_blocks_restarts, block_size>>> (tourMatrixIn_d, cities, restarts);
+    //transposeTiled<unsigned short, TILE>(tourMatrixIn_d, tourMatrixTrans_d, (cities+1), restarts);
     
     //Compute shared memory size
     size_t sharedMemSize = (cities+1) * sizeof(unsigned short) + 
@@ -167,8 +166,8 @@ void run_calculatedIandJ(unsigned short *tourMatrixIn_d,
     num_blocks_restarts = (restarts + block_size-1)/block_size;
     
     //Create randomized tours
-    createToursColumnWise<<<num_blocks_restarts, block_size>>> (tourMatrixIn_d, cities, restarts);
-    transposeTiled<unsigned short, TILE>(tourMatrixIn_d, tourMatrixTrans_d, (cities+1), restarts);
+    //createToursColumnWise<<<num_blocks_restarts, block_size>>> (tourMatrixIn_d, cities, restarts);
+    //transposeTiled<unsigned short, TILE>(tourMatrixIn_d, tourMatrixTrans_d, (cities+1), restarts);
     
     //Compute shared memory size
     size_t sharedMemSize = (cities+1) * sizeof(unsigned short) + 
