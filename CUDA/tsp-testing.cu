017234faa327ccbc51c7948c252c178c63f6d916
Henriette Hansen <henni_95@hotmail.com>
2023-03-20 13:02:53 +0100

    create testing

---
+++ CUDA/tsp-testing.cu
@@ -0,0 +1,25 @@
+#include "tsp-main-helper.cu.h"
+
+int main() {
+    int restarts, maxRestarts;
+    char* file_name = "../Data/hardcode.tsp";
+    initHwd();
+
+    restarts = 10;
+    maxRestarts = 100;
+    for(restars < maxRestarts){
+        printf("\nResults from version 1 w %d climbers: \n", restarts);
+        runProgram(file_name, restarts, 1);
+
+        printf("\nResults from version 2 w %d climbers: \n", restarts);
+        runProgram(file_name, restarts, 2);
+
+        printf("\nResults from version 3 w %d climbers: \n", restarts);
+        runProgram(file_name, restarts, 3);
+        
+        restarts = restarts * 10;
+    }
+   
+
+    return 0;
+}
