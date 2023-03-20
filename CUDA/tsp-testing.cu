cff631cdb5ac0186a6c9a3ef2c6d595cfac92657
Henriette Hansen <henni_95@hotmail.com>
2023-03-20 13:22:06 +0100

    Update tsp-testing.cu

--- CUDA/tsp-testing.cu
+++ CUDA/tsp-testing.cu
@@ -2,7 +2,7 @@
 
 int main() {
     int restarts, maxRestarts;
-    const *char file_name[21] = "../Data/hardcode.tsp";
+    const char* file_name = "../Data/hardcode.tsp";
     //const char* file_name[21] = ;
     initHwd();
 
