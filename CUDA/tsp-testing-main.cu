f60143462b83f6458bba353c625313afa1a236ee
Henriette Hansen <henni_95@hotmail.com>
2023-03-20 13:40:57 +0100

    Update tsp-testing-main.cu

--- CUDA/tsp-testing-main.cu
+++ CUDA/tsp-testing-main.cu
@@ -26,15 +26,8 @@ int main(int argc, char* argv[]) {
     initHwd();
 
     while(restarts < maxRestarts){
-        printf("\nResults from version 1 w %d climbers: \n", restarts);
+        printf("\n\nResult with %d climbers: \n", restarts);
         runProgram(file_name, restarts, 1);
-
-        printf("\nResults from version 2 w %d climbers: \n", restarts);
-        runProgram(file_name, restarts, 2);
-
-        printf("\nResults from version 3 w %d climbers: \n", restarts);
-        runProgram(file_name, restarts, 3);
-        
         restarts++;
     }
    
