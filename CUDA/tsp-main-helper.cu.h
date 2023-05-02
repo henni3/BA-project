eb0b5a5de5b5fc08ee1b2165b84177754db7cd43
Utne <johann.utne@gmail.com>
2023-05-02 11:54:34 +0200

    fixed bug

--- CUDA/tsp-main-helper.cu.h
+++ CUDA/tsp-main-helper.cu.h
@@ -370,7 +370,7 @@ void runProgram(char* file_name, int restarts, int version){
         gettimeofday(&start, NULL); 
         for(int i = 0; i < GPU_RUNS; i++){
             //run program
-            printf("we get here \n")
+            printf("we get here \n");
             init(block_size, cities, totIter, is_d, js_d);
             run_test(tourMatrixIn_d, tourMatrixTrans_d, 
                         is_d, kerDist, glo_results, counter, 
