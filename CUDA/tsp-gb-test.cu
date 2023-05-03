be5ddd66943fcfe812f985391246443f0aabf9ac
Utne <johann.utne@gmail.com>
2023-05-03 13:31:20 +0200

    change

--- CUDA/tsp-gb-test.cu
+++ CUDA/tsp-gb-test.cu
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <file-name> <number-of-restarts (multiple of 10)> \n", argv[0]);
+        printf("Usage: %s <file-name> <number-of-restarts (multiple of 50)> \n", argv[0]);
         exit(1);
     }
     // Collect input arguments
@@ -17,12 +17,9 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
     initHwd();
-    for (int i = 1; i <= restarts; i += 10) {
+    for (int i = 50; i <= restarts; i += 50) {
         runProgram(file_name, i, 4);
         //printf("iteration %d \n", i);
-        if (i == 1) {
-            i--;
-        }
     }
 
     return 0;
