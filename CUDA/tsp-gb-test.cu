a65c30c3e593dbdc38d056e50b0ddacf33c3f56c
Utne <johann.utne@gmail.com>
2023-05-04 13:48:52 +0200

    change

--- CUDA/tsp-gb-test.cu
+++ CUDA/tsp-gb-test.cu
@@ -1,13 +1,16 @@
 #include "tsp-main-helper.cu.h"
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <file-name> <number-of-restarts>\n", argv[0]);
+    if (argc < 3) {
+        printf("Usage: %s <number-of-restarts> <list of files>\n", argv[0]);
         exit(1);
     }
     // Collect input arguments
-    char* file_name = argv[1];
-    int restarts = atoi(argv[2]);
+    char* file_names[];
+    for( int i = 2; i < argc; i++) {
+        file_names[i-2] = argv[i];
+    }
+    int restarts = atoi(argv[1]);
     if(restarts <= 0){
         printf("Number of restarts has to be a number larger than 0");
         exit(1);
@@ -15,10 +18,14 @@ int main(int argc, char* argv[]) {
     //int cities = atoi(argv[3]);
     int jump = restarts / 10;
     initHwd();
-    printf("testing for %s, each run repeated 100 times and taken average time \n", file_name);
-    for (int i = jump; i <= restarts; i += jump) {
+    
+    for (char* dataset : file_names ) {
+         printf("testing for %s, each run repeated 100 times and taken average time \n", dataset);
+        for (int i = jump; i <= restarts; i += jump) {
         runProgram(file_name, i, 4);
         //printf("iteration %d \n", i);
+        printf("\n\n");
+    }
     }
 
     return 0;
